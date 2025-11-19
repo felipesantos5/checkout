e14287e173f48409bb46bc3583619b488fe65126
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2025-11-19 19:21:47 -0300

    change cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -7,44 +7,45 @@ import stripeWebhookRouter from "./webhooks/stripe/stripe-webhook.routes";
 
 const app: Express = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://localhost:5173",
-  "http://localhost:5174",
-  "https://localhost:5174",
-  "https://admin.snappcheckout.com",
-  "https://checkout.abatools.pro",
-  "https://abatools.pro",
-  "http://31.97.30.228:8088",
-  "https://snappcheckout.com",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://localhost:5173",
+//   "http://localhost:5174",
+//   "https://localhost:5174",
+//   "https://admin.snappcheckout.com",
+//   "https://checkout.abatools.pro",
+//   "https://abatools.pro",
+//   "http://31.97.30.228:8088",
+//   "https://snappcheckout.com",
+//   "https://pay.snappcheckout.com",
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // 1. Permite requisições sem 'origin' (como Postman, Insomnia ou chamadas server-to-server)
-      if (!origin) return callback(null, true);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       // 1. Permite requisições sem 'origin' (como Postman, Insomnia ou chamadas server-to-server)
+//       if (!origin) return callback(null, true);
 
-      // 2. Verifica se a origem está na whitelist
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
+//       // 2. Verifica se a origem está na whitelist
+//       if (allowedOrigins.includes(origin)) {
+//         return callback(null, true);
+//       }
 
-      // (Opcional) 3. Permite subdomínios dinâmicos (ex: deploy previews da Vercel)
-      if (origin.endsWith(".vercel.app")) {
-        return callback(null, true);
-      }
+//       // (Opcional) 3. Permite subdomínios dinâmicos (ex: deploy previews da Vercel)
+//       if (origin.endsWith(".vercel.app")) {
+//         return callback(null, true);
+//       }
 
-      console.log(`🚫 CORS Bloqueado: ${origin}`);
-      return callback(new Error("Bloqueado pelo CORS policy"));
-    },
-    credentials: true, // IMPORTANTE: Permite enviar Cookies e Headers de Auth
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "sentry-trace", "baggage"],
-  })
-);
+//       console.log(`🚫 CORS Bloqueado: ${origin}`);
+//       return callback(new Error("Bloqueado pelo CORS policy"));
+//     },
+//     credentials: true, // IMPORTANTE: Permite enviar Cookies e Headers de Auth
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "sentry-trace", "baggage"],
+//   })
+// );
 
-// app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" }));
 
 app.use("/api/webhooks/stripe", stripeWebhookRouter);
 
