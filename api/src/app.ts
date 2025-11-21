b00576087f0e2465c58c4c102cf46dd9388895f9
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2025-11-21 02:51:31 -0300

    fix(api): cors config

--- api/src/app.ts
+++ api/src/app.ts
@@ -9,7 +9,7 @@ const app: Express = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://checkout.seudominio.com"],
+    origin: ["*"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
