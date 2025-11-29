18937cf314b109eecb449d866a0b046a9dbaac1c
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2025-11-29 09:15:06 -0300

    feat: add cache offer route

--- api/src/controllers/offer.controller.ts
+++ api/src/controllers/offer.controller.ts
@@ -32,6 +32,14 @@ export const handleGetOfferBySlug = async (req: Request, res: Response) => {
       return res.status(404).json({ error: { message: "Oferta não encontrada." } });
     }
 
+    // --- IMPLEMENTAÇÃO DO CACHE ---
+    // public: qualquer um pode cachear (CDN, Browser)
+    // max-age=60: O navegador do cliente guarda por 60 segundos (1 min)
+    // s-maxage=600: A CDN da Vercel guarda por 600 segundos (10 min)
+    // stale-while-revalidate=30: Aceita dado vencido por +30s enquanto renova em background
+    res.set("Cache-Control", "public, max-age=60, s-maxage=600, stale-while-revalidate=30");
+    // ------------------------------
+
     res.status(200).json(offer);
   } catch (error) {
     res.status(500).json({ error: { message: (error as Error).message } });
